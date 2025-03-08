efc402b8ba86ce4747fd4164c0954d1acf0619af
Joochan <joochan123123@gmail.com>
2025-03-08 22:05:06 +0900

    feat: 자료형 기본타입 T접두사로 using 선언.

--- GTLEngine/Source/Utils/EnginePch.h
+++ GTLEngine/Source/Utils/EnginePch.h
@@ -9,6 +9,9 @@
 #include <array>
 #include <list>
 #include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
 using namespace std;
 
 // D3D include
@@ -22,7 +25,7 @@ using namespace std;
 
 using namespace Microsoft::WRL;
 
-// using typedef
+// typedef default int type.
 typedef UINT		uint;
 typedef	UINT8		uint8;
 typedef	UINT16		uint16;
@@ -32,3 +35,19 @@ typedef	INT8		int8;
 typedef	INT16		int16;
 typedef	INT32		int32;
 typedef	INT64		int64;
+
+template <typename T>
+using TArray = vector<T>;
+
+template <typename T1, typename T2>
+using TPair = pair<T, T2>;
+
+template <typename T, typename T2>
+using TMap = unordered_map<T, T2>;
+
+template <typename T>
+using TQueue = queue<T>;
+
+template <typename T>
+using TSet = set<T>;
+
