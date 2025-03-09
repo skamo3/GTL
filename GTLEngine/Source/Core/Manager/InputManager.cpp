6a1ecc832354eafa502aa97d80ba2079b61ed31c
Mstone8370 <mstone8370@gmail.com>
2025-03-09 14:20:44 +0900

    feat: InputManager 추가

---
+++ GTLEngine/Source/Core/Manager/InputManager.cpp
@@ -0,0 +1,102 @@
+
+#include "InputManager.h"
+
+UInputManager::UInputManager()
+    : currentKeyStates(TArray<bool>(256, false))
+    , prevKeyStates(TArray<bool>(256, false))
+{}
+
+UInputManager::~UInputManager()
+{}
+
+void UInputManager::Update(HWND hWnd, int Width, int Height)
+{
+    // 현재 키 상태를 이전 상태에 복사
+    prevKeyStates = currentKeyStates;
+    // 0~255 범위의 가상키에 대해 GetAsyncKeyState를 호출하여 현재 상태 업데이트
+    for (int i = 0; i < 256; ++i)
+    {
+        currentKeyStates[i] = (GetAsyncKeyState(i) & 0x8000) != 0;
+    }
+
+    // 현재 마우스 상태를 이전 상태에 복사
+    prevMouseState = currentMouseState;
+    // 마우스 버튼 상태 업데이트
+    currentMouseState.leftButton = (GetAsyncKeyState(VK_LBUTTON) & 0x8000) != 0;
+    currentMouseState.rightButton = (GetAsyncKeyState(VK_RBUTTON) & 0x8000) != 0;
+    currentMouseState.middleButton = (GetAsyncKeyState(VK_MBUTTON) & 0x8000) != 0;
+
+    // 마우스 커서 위치 업데이트
+    POINT pt;
+    if (GetCursorPos(&pt))
+    {
+        currentMouseState.ScreenX = pt.x;
+        currentMouseState.ScreenY = pt.y;
+
+        if (ScreenToClient(hWnd, &pt))
+        {
+            currentMouseState.ClientX = pt.x;
+            currentMouseState.ClientY = pt.y;
+
+            ConvertMouseToNDC(hWnd, Width, Height);
+        }
+    }
+}
+
+bool UInputManager::GetKey(int key) const
+{
+    return currentKeyStates[key];
+}
+
+bool UInputManager::GetKeyDown(int key) const
+{
+    return currentKeyStates[key] && !prevKeyStates[key];
+}
+
+bool UInputManager::GetKeyUp(int key) const
+{
+    return !currentKeyStates[key] && prevKeyStates[key];
+}
+
+void UInputManager::ConvertMouseToNDC(HWND hWnd, int Width, int Height)
+{
+    // 마우스 커서 위치를 NCD 좌표로 변환
+    float HalfWidth = Width / 2.f;
+    float HalfHeight = Height / 2.f;
+    float MaxHalfLength = max(HalfWidth, HalfHeight);
+    currentMouseState.ndcX = (currentMouseState.ScreenX - HalfWidth) / HalfWidth;
+    currentMouseState.ndcY = (currentMouseState.ScreenY - HalfHeight) / HalfWidth * -1.f;
+}
+
+bool UInputManager::GetMouseButton(int button) const
+{
+    if (button == 0)
+        return currentMouseState.leftButton;
+    if (button == 1)
+        return currentMouseState.rightButton;
+    if (button == 2)
+        return currentMouseState.middleButton;
+    return false;
+}
+
+bool UInputManager::GetMouseDown(int button) const
+{
+    if (button == 0)
+        return currentMouseState.leftButton && !prevMouseState.leftButton;
+    if (button == 1)
+        return currentMouseState.rightButton && !prevMouseState.rightButton;
+    if (button == 2)
+        return currentMouseState.middleButton && !prevMouseState.middleButton;
+    return false;
+}
+
+bool UInputManager::GetMouseUp(int button) const
+{
+    if (button == 0)
+        return !currentMouseState.leftButton && prevMouseState.leftButton;
+    if (button == 1)
+        return !currentMouseState.rightButton && prevMouseState.rightButton;
+    if (button == 2)
+        return !currentMouseState.middleButton && prevMouseState.middleButton;
+    return false;
+}
---
+++ GTLEngine/Source/Core/Manager/InputManager.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "pch.h"
+
+class UInputManager
+{
+public:
+    static UInputManager& Instance()
+    {
+        static UInputManager instance;
+        return instance;
+    }
+
+    // 매 프레임 호출하여 입력 상태를 업데이트
+    void Update(HWND hWnd, int Width, int Height);
+
+    // 키보드 입력 함수
+    bool GetKey(int key) const;
+    bool GetKeyDown(int key) const;
+    bool GetKeyUp(int key) const;
+
+    // 마우스 상태 구조체
+    struct MouseState
+    {
+        bool leftButton = false;
+        bool rightButton = false;
+        bool middleButton = false;
+        int ScreenX = 0;
+        int ScreenY = 0;
+        int ClientX = 0;
+        int ClientY = 0;
+        float ndcX = 0.f;
+        float ndcY = 0.f;
+    };
+
+    // 마우스 입력 관련 함수
+    bool GetMouseButton(int button) const;
+    bool GetMouseDown(int button) const;
+    bool GetMouseUp(int button) const;
+    int GetMouseClientX() const { return currentMouseState.ScreenX;};
+    int GetMouseClient() const { return currentMouseState.ScreenY;};
+    int GetMouseScreenX() const { return currentMouseState.ClientX;};
+    int GetMouseScreenY() const { return currentMouseState.ClientY;};
+    float GetMouseNDCX() const { return currentMouseState.ndcX;};
+    float GetMouseNDCY() const { return currentMouseState.ndcY;};
+    int GetMouseDeltaX() const { return currentMouseState.ClientX - prevMouseState.ClientX; };
+    int GetMouseDeltaY() const { return currentMouseState.ClientY - prevMouseState.ClientY; };
+
+
+protected:
+    void ConvertMouseToNDC(HWND hWnd, int Width, int Height);
+
+private:
+    // 생성자, 소멸자, 복사/대입 금지
+    UInputManager();
+    ~UInputManager();
+    UInputManager(const UInputManager&) = delete;
+    UInputManager& operator=(const UInputManager&) = delete;
+    UInputManager(UInputManager&&) = delete;
+    UInputManager& operator=(UInputManager&&) = delete;
+
+    TArray<bool> currentKeyStates;
+    TArray<bool> prevKeyStates;
+
+    MouseState currentMouseState;
+    MouseState prevMouseState;
+};
